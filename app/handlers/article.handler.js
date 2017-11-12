529afb7e5aca4c98acef81a8e38f060271e04b71
Lex Martinez <lex0316@gmail.com>
2017-11-11 21:33:17 -0500

    tag - article routes

---
+++ app/handlers/article.handler.js
@@ -0,0 +1,17 @@
+module.exports = {
+  list: (request, reply) => {
+    reply('Hello Handler')
+  },
+  find: (request, reply) => {
+    reply('Hello Handler')
+  },
+  store: (request, reply) => {
+    reply('Hello Handler')
+  },
+  update: (request, reply) => {
+    reply('Hello Handler')
+  },
+  delete: (request, reply) => {
+    reply('Hello Handler')
+  }
+}
---
+++ app/routes/article.routes.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const handler = require('../handlers/article.handler')
+const prefix = '/articles'
+
+module.exports = [
+  {
+    method: 'GET',
+    path: prefix,
+    handler: handler.list
+  },
+  {
+    method: 'GET',
+    path: prefix + '/{id}',
+    handler: handler.find
+  },
+  {
+    method: 'POST',
+    path: prefix,
+    handler: handler.store
+  },
+  {
+    method: ['PUT', 'PATCH'],
+    path: prefix + '/{id}',
+    handler: handler.update
+  },
+  {
+    method: 'DELETE',
+    path: prefix + '/{id}',
+    handler: handler.delete
+  }
+]
